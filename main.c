55ccfa6a5be976174a804f57564e7c78e1a1f0b2
Agustin <darkstone19@gmail.com>
2023-04-19 21:57:45 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/*
+ EJERCICIO 17 : Defina una función que pide el tamaño de dos conjuntos y luego de cargarlos devuelve la intersección entre ambos.
+ */
 int main() {
     printf("Hello, World!\n");
     return 0;
